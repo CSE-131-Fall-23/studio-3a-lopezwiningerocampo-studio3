90d5952888dbc67aef4f901d04892d11db717d55
maria <maria@mariafl12.dhcp.wustl.edu>
2023-09-20 14:11:39 -0500

    please finish

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,21 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner in = new Scanner (System.in);
+System.out.println("Enter n ");
+ int n = in.nextInt();
+		boolean [] sieve = {n};
+		for (int i = 0; i < n; i++){
+			System.out.println(sieve[i]);
+			if (i%2==0) {
+				i = false;
+			}
+		}
+	}
+
+}
